d7f2c78b1d654d21778e2d46dd429f944ca9ce6f
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-11-01 21:15:15 -0300

    configure vite, ts, tailwind, postcss

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'utfpr-yellow': '#ffbf00',
+        'utfpr-gray': '#8d9199',
+        'background': '#fafdfd',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
