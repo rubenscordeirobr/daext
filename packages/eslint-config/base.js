8e88ca39d4ed6b14f34be90326f1794c27e9d87e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-11-05 19:21:12 -0300

    feat: update package.json, tsconfig.json for apps and packages, add api-client and domain workspaces

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,24 +1,26 @@
-import js from "@eslint/js"
-import globals from "globals"
-import tseslint from "typescript-eslint"
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export const base = tseslint.config(
-  {
-    ignores: ["dist", "build", "out", ".turbo", "node_modules"]
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: true
-        }
-      },
-      globals: {
-        ...globals.es2021
-      }
+    {
+        ignores: ['dist', 'build', 'out', '.turbo', 'node_modules'],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+            globals: {
+                ...globals.es2021,
+            },
+        },
+        rules: {
+            semi: ['error', 'always'],
+            indent: ['error', 4, { SwitchCase: 1, VariableDeclarator: 1 }],
+        },
     }
-  }
-)
+);
